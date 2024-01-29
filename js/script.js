0076fb563e9085ff257714d951c06d1d10e3e210
katya-pankov <e.pankov@students.clark.edu>
2024-01-29 14:14:18 -0800

    Added pagination

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ const global = {
         term: '',
         type: '',
         page: 1,
-        totalPages: 1
+        totalPages: 1,
+        totalResults: 0
     },
     api: {
         apiKey: 'e931f5a575f809327531490b57a0f8b2',
@@ -191,7 +192,7 @@ function displayBackgroundImage(type, backgroundPath) {
     }
 }
 
-// Serach moves/shows
+// Serach movies/shows
 async function search() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
@@ -199,7 +200,11 @@ async function search() {
     global.search.term = urlParams.get('search-term');
 
     if (global.search.term !== '' && global.search.term !== null) {
-        const { results, total_pages, page } = await searchAPIData();
+        const { results, total_pages, page, total_results } = await searchAPIData();
+
+        global.search.page = page;
+        global.search.totalPages = total_pages;
+        global.search.totalResults = total_results;
 
         if (results.length === 0) {
             showAlert('No results found');
@@ -216,6 +221,11 @@ async function search() {
 }
 
 function displaySearchResults(results) {
+    // Clear previous results
+    document.querySelector('#search-results').innerHTML = '';
+    document.querySelector('#search-results-heading').innerHTML = '';
+    document.querySelector('#pagination').innerHTML = '';
+
     results.forEach((result) => {
         const div = document.createElement('div');
         div.classList.add('card');
@@ -233,9 +243,51 @@ function displaySearchResults(results) {
         </div>
         `;
 
-        document.querySelector('#search-results').appendChild(div);
-    })
+        document.querySelector('#search-results-heading').innerHTML = `
+        <h2>${results.length} of ${global.search.totalResults} results for "${global.search.term}"</h2>
+        `;
 
+        document.querySelector('#search-results').appendChild(div);
+
+
+    })
+    displayPagination();
+}
+
+// Create & Display Pagination For Search
+function displayPagination() {
+    const div = document.createElement('div');
+    div.classList.add('pagination');
+    div.innerHTML = `
+    <button class="btn btn-primary" id="prev">Prev</button>
+    <button class="btn btn-primary" id="next">Next</button>
+    <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+    `;
+
+    document.querySelector('#pagination').appendChild(div);
+
+    // Disable prev button if on first page
+    if (global.search.page === 1) {
+        document.querySelector('#prev').disabled = true;
+    }
+
+    // Disable the next button if on last page
+    if (global.search.page === global.search.totalPages) {
+        document.querySelector('#next').disabled = true;
+    }
+
+    //Next page
+    document.querySelector('#next').addEventListener('click', async () => {
+        global.search.page++;
+        const { results, total_pages } = await searchAPIData();
+        displaySearchResults(results);
+    });
+    //Prev page
+    document.querySelector('#prev').addEventListener('click', async () => {
+        global.search.page--;
+        const { results, total_pages } = await searchAPIData();
+        displaySearchResults(results);
+    });
 }
 // Display Slider Movies
 async function displaySlider() {
@@ -309,7 +361,7 @@ async function searchAPIData() {
 
     showSpinner();
 
-    const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`);
+    const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`);
 
     const data = await response.json();
 
